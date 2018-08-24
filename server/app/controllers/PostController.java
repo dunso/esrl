335e070c5e8d2595437ee0c00d18a5fcb26a1a95
dunso <dunso@dun.so>
2018-08-24 23:46:11 +0800

    Add PostController.java

---
+++ server/app/controllers/PostController.java
@@ -0,0 +1,91 @@
+package controllers;
+
+import annotations.Authenticated;
+import annotations.PostExistsAndUserIsOwner;
+import com.google.common.base.Strings;
+import com.typesafe.config.Config;
+import dto.PostDTO;
+import dto.Response;
+import enums.CustomCode;
+import enums.PostStatus;
+import play.data.Form;
+import play.data.FormFactory;
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
+import services.PostService;
+import utils.PostsPager;
+import utils.Tools;
+
+import javax.inject.Inject;
+
+public class PostController extends Controller {
+
+    @Inject
+    public Config config;
+    @Inject
+    public FormFactory formFactory;
+    @Inject
+    public PostService postService;
+
+    public Result getPagePosts(Integer pageSize, Integer currentPage, PostStatus postStatus) {
+        PostsPager postsPager = postService.getPagePosts(
+                pageSize, currentPage, postStatus != null ? postStatus : null
+        );
+        return ok(Json.toJson(Response.success(postsPager)));
+    }
+
+    public Result getPagePostsByNickName(Integer pageSize, Integer currentPage, PostStatus postStatus, String email) {
+        if (email == null) {
+            email = session().get("email");
+        }
+        PostsPager postsPager = postService.getPagePostsByNickName(
+                pageSize, currentPage, postStatus != null ? postStatus : null, email
+        );
+        return ok(Json.toJson(Response.success(postsPager)));
+    }
+
+    public Result getPostById(Long id) {
+        return postService.getPostById(id)
+                .map(post -> ok(Json.toJson(Response.success(post))))
+                .orElse(notFound(Json.toJson(new Response(CustomCode.ARTICLE_NOT_FOUND))));
+    }
+
+    @Authenticated
+    public Result createPost() {
+        Form<PostDTO> postForm = formFactory.form(PostDTO.class).bindFromRequest();
+        if (postForm.hasErrors()) {
+            return badRequest(postForm.errorsAsJson());
+        }
+
+        PostDTO postDTO = postForm.get();
+        String email = session("email");
+        if (Strings.isNullOrEmpty(email)) {
+            return forbidden(Json.toJson(Response.forbidden()));
+        }
+
+        postService.savePost(postDTO, email);
+
+        return ok(Json.toJson(Response.success()));
+    }
+
+    @Authenticated
+    @PostExistsAndUserIsOwner
+    public Result editPost(Long postId) {
+        Form<PostDTO> postForm = formFactory.form(PostDTO.class).bindFromRequest();
+        if (postForm.hasErrors()) {
+            return badRequest(postForm.errorsAsJson());
+        }
+        PostDTO postDTO = postForm.get();
+        postDTO.id = postId;
+        postService.updatePost(postDTO);
+        return ok(Json.toJson(Response.success()));
+    }
+
+    @Authenticated
+    @PostExistsAndUserIsOwner
+    public Result deletePost(Long postId) {
+        postService.deletePost(postId);
+        return ok(Json.toJson(Response.success()));
+    }
+}
